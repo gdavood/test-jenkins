257e6a655440fca49971c95548efd2b6e1455039
gdavood <dav02306@gmail.com>
2024-04-02 09:51:51 +0530

    main.tf commit

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+    region = "ap-south-1"
+}
+
+resource "aws_instance" "test1" {
+    ami = "ami-007020fd9c84e18c7"
+    instance_type = "t2.micro"
+    tags = {
+        Name = Test-Jenkins
+    }
+  
+}
